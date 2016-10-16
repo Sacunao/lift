05c000711c57c710351f0d32596b745bacc53b11
sacunao <sheila.aobregon@gmail.com>
2016-10-15 20:35:29 -0500

    validando código y número de localStorage

---
+++ js/prueba.js
@@ -0,0 +1,59 @@
+function validateForm(){
+	/* Escribe tú código aquí */
+	var nombre = document.getElementById("name");
+	var apellido = document.getElementById("lastname");
+	var correo = document.getElementById("input-email");
+	var contra = document.getElementById("input-password");
+	var tipoBici = document.getElementById("tipo-bici");
+
+	// Campos obligatorios
+	if (nombre.value.trim().length == 0 ||
+		apellido.value.trim().length == 0 ||
+		correo.value.trim().length == 0 ||
+		contra.value.trim().length == 0 ||
+		tipoBici.value == 0) {
+		alert("Ingrese todos los campos obligatorios");
+	}
+
+	// A-Z para nombres y apellidos
+	var regexNombre = /^[a-zñáéíóúü]+$/gi;
+	if (!regexNombre.test(nombre.value)) {
+		alert("Ingresa un nombre válido");
+	}
+
+	var regexApellido = /^[a-zñáéíóúü]+$/gi;
+	if (!regexApellido.test(apellido.value)) {
+		alert("Ingresa un apellido válido");
+	}
+
+
+	// Primera letra mayúscula para los campos nombres y apellidos
+	if (nombre.value[0] == nombre.value[0].toLowerCase()) {
+		alert("Ingrese la primera letra del nombre en mayúscula");
+	}
+	if (apellido.value[0] == apellido.value[0].toLowerCase()) {
+		alert("Ingrese la primera letra del apellido en mayúscula");
+	}
+
+	// Formato de Correo
+	var regexCorreo = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+	if (!regexCorreo.test(correo.value)) {
+		alert("Ingresa un correo válido");
+	}
+
+	// Password de al menos 6 caracteres
+	if (contra.value.length < 6) {
+		alert("Ingresa un password decente ¬¬");
+	}
+
+	// Password no igual a "password", "123456", "098754"
+	if (contra.value == "password" || contra.value == "123456" || contra.value == "098754") {
+		alert("Ingresa un password decente ¬¬!");
+	}
+
+	// El valor seleccionado de bicis, debe ser una de las opciones presentadas
+	if (tipoBici.value != "urbana" && tipoBici.value != "treking" && tipoBici.value != "electrica" && tipoBici.value != "estatica") {
+		alert("Escoge bien el tipo de bici oe!");
+	}
+
+}
--- js/signup.js
+++ js/signup.js
@@ -2,6 +2,22 @@ $(document).ready(function() {
 	$("#inputNumero").keydown(validandoTeclas);
 	$("#inputNumero").keyup(longitud);
 	$("#nextstep").click(creandoCodigo);
+	$("#nextstep").click(validandoEmail);
+	$(".codenumber").keydown(validandoTeclas);
+	$(".codenumber").keyup(asignandoFocus);
+	$(".resend").click(reenviarCodigo);
+	$("#nextstep2").click(validandoCodigo);
+
+	
+	function validandoEmail() {
+	  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	  if($("#email").val() == regex){
+	  	console.log("hola");
+	  } else {
+	  	console.log("no");
+	  }
+	}
+
 
 	function validandoTeclas(evento){
 		var ascii = evento.keyCode;
@@ -16,6 +32,8 @@ $(document).ready(function() {
 		var longitud = $(this).val().length;
 		if (longitud == 9) {
 			$("#nextstep").attr("href", "signupdos.html");
+			var movil =  $("#inputNumero").val();
+			localStorage.setItem("numeroMovil", movil);
 		} else if ((longitud > 9)){
 			$("#nextstep").removeAttr("href");
 			sweetAlert("Error...", "ingrese nùmero válido de 9 dígitos", "error");
@@ -24,14 +42,50 @@ $(document).ready(function() {
 		}
 	}
 
+	var movilNumber = localStorage.getItem("numeroMovil");
+	$("#movil").text(movilNumber);
+
 	function creandoCodigo(e){
 		var numero = $("#inputNumero").val().length;
 		var numeroRandom = Math.round(Math.random()*900) + 99;
 		if(numero == 9){
 			alert("LAB - " + numeroRandom);
-			$("#nextstep").attr("href", "signupdos.html");
+			$(this).attr("href", "signupdos.html");	
 		}
+
+		localStorage.setItem("codigo",numeroRandom);
 	}
+
+	var codigoEntregado = localStorage.getItem("codigo");
+
+	function asignandoFocus(e){
+	  var llave = e.keyCode;
+      if($(this).val().length == $(this).attr("maxlength")){
+          $(this).next().focus();
+      } else if(llave == 8){
+      	  $(this).prev().focus();
+      }
+  	}
+
+  	function reenviarCodigo(e){
+  		swal("Por si lo olvidaste", codigoEntregado, "success");
+  	}
+
+  	function validandoCodigo(e){
+  		var codeInputs = $(".codenumber").eq(0).val() + $(".codenumber").eq(1).val() + $(".codenumber").eq(2).val();
+  		if (codeInputs == codigoEntregado){
+  			$(this).attr("href", "signuptres.html");	
+  		} else if (codeInputs != codigoEntregado) {
+  			sweetAlert("Error...", "El código ingresado incompleto o incorrecto.", "error");
+  		} 
+  	}
+
+  // 	function validandoNombres(){
+		// var regexNombre = /^[a-zñáéíóúü]+$/gi;
+		// if (!regexNombre.test( $(this).val()){
+		// 	alert("Ingresa un nombre válido");
+		// }
+  // 	}
 
 });
 
