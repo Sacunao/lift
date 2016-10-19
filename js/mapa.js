a37ea3f7aec2d8a74bbe18a5d4b62c060206f8ff
sheila acuña <sheila acua>
2016-10-19 13:57:05 -0500

    agregando mapa

--- js/mapa.js
+++ js/mapa.js
@@ -1,41 +1,33 @@
 $(document).ready(function(e){
-     {
     if (navigator.geolocation) { 
-        // también se puede usar if ("geolocation" in navigator) {}
-        navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+       navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
     }
-};
 
-    function funcionExito(posicion) {
-        var lat = posicion.coords.latitude;
-        var lon = posicion.coords.longitude;
-        var latlon = new google.maps.LatLng(lat, lon)
-        var mapa = document.getElementById('mapa')
-        mapa.style.height = '250px';
-        mapa.style.width = '500px';
+    
+   function funcionExito(posicion) {
+    var lat = posicion.coords.latitude;
+    var lon = posicion.coords.longitude;
+    var maps = new GMaps({
+          div: "#mapa",
+          lat: lat ,
+          lng: lon ,
+          mapTypeControl: false,
+          zoomControl: false,
+          streetViewControl: false
+        });
 
-        var myOptions = {
-            center:latlon,zoom:14,
-            mapTypeId:google.maps.MapTypeId.ROADMAP,
-            mapTypeControl:false,
-            navigationControlOptions:{
-                style: google.maps.NavigationControlStyle.SMALL
-            }
-        };
-        
-        var map = new google.maps.Map(document.getElementById('mapa'), myOptions);
-
-        var marker = new google.maps.Marker({
-            position:latlon,
-            map:map,
-            title:"You are here!"
+        maps.addMarker({
+          lat: lat ,
+          lng: lon ,
+          title: 'Lima',
+          click: function(e) {
+            alert('You clicked in this marker');
+          }
         });
     };
 
-    function funcionError  (error) {
-        console.log(error);
+    function funcionError(error) {
+        alert("Error: Usted tiene que aceptar la solicitud de ubicación");
     };
-
-
 
 });
